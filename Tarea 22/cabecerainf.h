16078185bee305b826081e588a8130f95b2d9e55
Irving Ricardo Juárez Vázquez <31520357+irvjuava@users.noreply.github.com>
2017-11-15 00:29:16 -0600

    Add files via upload

---
+++ Tarea 22/cabecerainf.h
@@ -0,0 +1,11 @@
+void appInfoData(char *titulo,char *fecha)
+{
+	printf("----------------------------------------------------------------\n");
+	printf("_\n");
+	printf("_       %s\n", titulo);
+	printf("_       Juárez Vázquez Irving Ricardo. ®\n");
+	printf("_       %s\n", fecha);
+	printf("_\n");
+	printf("----------------------------------------------------------------\n");
+}
+
